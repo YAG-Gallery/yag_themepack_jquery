1797cad03ac539e5065f5322d4ae292ec666a787
Daniel Lienert <daniel@lienert.cc>
2011-03-23 16:22:15 +0100

    ADD: all options to jsTemplate and listTemplate

--- Configuration/TypoScript/Themes/GalleryView/General.ts
+++ Configuration/TypoScript/Themes/GalleryView/General.ts
@@ -20,4 +20,18 @@ plugin.tx_yag.settings.themes.galleryView {
 			list.template = EXT:yag_themepack_jquery/Resources/Private/Templates/GalleryView/ItemList/List.html
 		}
 	}
+	
+	includeLibJS = jQuery,jQueryUi
+	includeLibCSS = jQueryUi
+	
+	includeJS {
+		10 = EXT:yag_themepack_jquery/Resources/Public/GallerySource/GalleryView/js/jquery.timers-1.2.js
+		20 = EXT:yag_themepack_jquery/Resources/Public/GallerySource/GalleryView/js/jquery.easing.1.3.js
+		30 = EXT:yag_themepack_jquery/Resources/Public/GallerySource/GalleryView/js/jquery.galleryview-3.0.js
+	}
+	
+	includeCSS >
+	includeCSS {
+		10 = EXT:yag_themepack_jquery/Resources/Public/GallerySource/GalleryView/css/jquery.galleryview-3.0.css
+	}
 }
