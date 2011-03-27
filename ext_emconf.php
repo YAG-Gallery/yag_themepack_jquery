646c25219fbc0a090e3f4a611df7f56833573507
Daniel Lienert <daniel@lienert.cc>
2011-03-27 17:34:16 +0200

    RELEASE V0.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,9 +1,9 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext "yag_theme_simpleviewer".
+# Extension Manager/Repository config file for ext "yag_themepack_jquery".
 #
-# Auto generated 15-03-2011 13:30
+# Auto generated 27-03-2011 17:05
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,13 +29,13 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '0.0.0',
+	'version' => '0.0.1',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
 			'extbase' => '',
 			'fluid' => '',
-			'yag' => '1.0.7',
+			'yag' => '1.1.1',
 		),
 		'conflicts' => array(
 		),
@@ -44,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:24:{s:12:"ext_icon.gif";s:4:"e6c8";s:17:"ext_localconf.php";s:4:"c867";s:14:"ext_tables.php";s:4:"8854";s:14:"ext_tables.sql";s:4:"d41d";s:16:"kickstarter.json";s:4:"c1b6";s:38:"Configuration/TypoScript/constants.txt";s:4:"eb6b";s:34:"Configuration/TypoScript/setup.txt";s:4:"aa4f";s:55:"Configuration/TypoScript/Themes/SimpleViewer/General.ts";s:4:"18fc";s:56:"Configuration/TypoScript/Themes/SimpleViewer/ItemList.ts";s:4:"03ea";s:53:"Configuration/TypoScript/Themes/SimpleViewer/Theme.ts";s:4:"f221";s:40:"Resources/Private/Language/locallang.xml";s:4:"ef62";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"046a";s:46:"Resources/Private/Templates/ItemList/List.html";s:4:"e7d0";s:49:"Resources/Private/Templates/ItemList/XmlList.html";s:4:"b7de";s:39:"Resources/Public/SimpleViewer/full.html";s:4:"1feb";s:50:"Resources/Public/SimpleViewer/css/simpleviewer.css";s:4:"2959";s:44:"Resources/Public/SimpleViewer/img/assets.png";s:4:"8b9e";s:48:"Resources/Public/SimpleViewer/img/caption-bg.png";s:4:"fb78";s:49:"Resources/Public/SimpleViewer/img/empty-pixel.png";s:4:"35a1";s:45:"Resources/Public/SimpleViewer/img/spinner.gif";s:4:"76ce";s:52:"Resources/Public/SimpleViewer/js/jquery-1.4.4.min.js";s:4:"73a9";s:48:"Resources/Public/SimpleViewer/js/simpleviewer.js";s:4:"88da";s:45:"Resources/Public/SimpleViewer/js/swfobject.js";s:4:"892a";s:50:"Resources/Public/SimpleViewer/swf/simpleviewer.swf";s:4:"fc38";}',
+	'_md5_values_when_last_written' => 'a:38:{s:12:"ext_icon.gif";s:4:"fb70";s:17:"ext_localconf.php";s:4:"c8ba";s:14:"ext_tables.php";s:4:"71ad";s:14:"ext_tables.sql";s:4:"d41d";s:38:"Configuration/TypoScript/constants.txt";s:4:"3f95";s:34:"Configuration/TypoScript/setup.txt";s:4:"4de7";s:54:"Configuration/TypoScript/Themes/GalleryView/General.ts";s:4:"4194";s:55:"Configuration/TypoScript/Themes/GalleryView/ItemList.ts";s:4:"23b2";s:52:"Configuration/TypoScript/Themes/GalleryView/Theme.ts";s:4:"22d0";s:40:"Resources/Private/Language/locallang.xml";s:4:"ef62";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"046a";s:58:"Resources/Private/Templates/GalleryView/ItemList/List.html";s:4:"8581";s:66:"Resources/Private/Templates/GalleryView/JSTemplates/GalleryView.js";s:4:"b10b";s:56:"Resources/Public/GallerySource/GalleryView/CHANGELOG.txt";s:4:"3fc6";s:54:"Resources/Public/GallerySource/GalleryView/LICENSE.txt";s:4:"87c7";s:53:"Resources/Public/GallerySource/GalleryView/README.txt";s:4:"1d86";s:73:"Resources/Public/GallerySource/GalleryView/css/jquery.galleryview-3.0.css";s:4:"5884";s:57:"Resources/Public/GallerySource/GalleryView/css/loader.gif";s:4:"c420";s:67:"Resources/Public/GallerySource/GalleryView/css/themes/dark/next.png";s:4:"6357";s:77:"Resources/Public/GallerySource/GalleryView/css/themes/dark/panel-nav-next.png";s:4:"2297";s:77:"Resources/Public/GallerySource/GalleryView/css/themes/dark/panel-nav-prev.png";s:4:"2f1b";s:67:"Resources/Public/GallerySource/GalleryView/css/themes/dark/prev.png";s:4:"197a";s:71:"Resources/Public/GallerySource/GalleryView/css/themes/ie6_dark/next.gif";s:4:"5fbc";s:81:"Resources/Public/GallerySource/GalleryView/css/themes/ie6_dark/panel-nav-next.gif";s:4:"d4c2";s:81:"Resources/Public/GallerySource/GalleryView/css/themes/ie6_dark/panel-nav-prev.gif";s:4:"45d8";s:71:"Resources/Public/GallerySource/GalleryView/css/themes/ie6_dark/prev.gif";s:4:"2d4f";s:72:"Resources/Public/GallerySource/GalleryView/css/themes/ie6_light/next.gif";s:4:"f3e2";s:82:"Resources/Public/GallerySource/GalleryView/css/themes/ie6_light/panel-nav-next.gif";s:4:"e254";s:82:"Resources/Public/GallerySource/GalleryView/css/themes/ie6_light/panel-nav-prev.gif";s:4:"e65b";s:72:"Resources/Public/GallerySource/GalleryView/css/themes/ie6_light/prev.gif";s:4:"df77";s:68:"Resources/Public/GallerySource/GalleryView/css/themes/light/next.png";s:4:"c3bf";s:78:"Resources/Public/GallerySource/GalleryView/css/themes/light/panel-nav-next.png";s:4:"8f5d";s:78:"Resources/Public/GallerySource/GalleryView/css/themes/light/panel-nav-prev.png";s:4:"e0b3";s:68:"Resources/Public/GallerySource/GalleryView/css/themes/light/prev.png";s:4:"9c45";s:66:"Resources/Public/GallerySource/GalleryView/js/jquery.easing.1.3.js";s:4:"a6f7";s:71:"Resources/Public/GallerySource/GalleryView/js/jquery.galleryview-3.0.js";s:4:"2483";s:75:"Resources/Public/GallerySource/GalleryView/js/jquery.galleryview-3.0.min.js";s:4:"2550";s:66:"Resources/Public/GallerySource/GalleryView/js/jquery.timers-1.2.js";s:4:"6c03";}',
 );
 
 ?>
