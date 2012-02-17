01ff10205351a22ba57ff5de1356c68cb04ee404
Daniel Lienert <daniel@lienert.cc>
2012-02-17 08:23:23 +0100

    CHG: Moved Javascript for rondell from the template to an JSTemplate

---
+++ Classes/ViewHelpers/RondellViewHelper.php
@@ -0,0 +1,73 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2010 Daniel Lienert <daniel@lienert.cc>, Michael Knoll <knoll@punkt.de>
+*  All rights reserved
+*
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Class 
+ * 
+ * @author Daniel Lienert <daniel@lienert.cc>
+ * @package ViewHelpers
+ */
+class Tx_YagThemepackJquery_ViewHelpers_RondellViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractTagBasedViewHelper {
+	
+	
+	/**
+	 * @var Tx_Yag_Domain_Configuration_ConfigurationBuilder
+	 */
+	protected $configurationBuilder;
+
+
+	/**
+	 * @var Tx_Yag_Utility_HeaderInclusion
+	 */
+	protected $headerInclusion;
+
+
+	/**
+	 * @var string
+	 */
+	protected $contextIdentifier;
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see Classes/Core/ViewHelper/Tx_Fluid_Core_ViewHelper_AbstractTagBasedViewHelper::initialize()
+	 */
+	public function initialize() {
+		parent::initialize();
+		$this->configurationBuilder =  Tx_Yag_Domain_Configuration_ConfigurationBuilderFactory::getInstance();
+		$this->headerInclusion = t3lib_div::makeInstance('Tx_Extbase_Object_ObjectManager')->get('Tx_Yag_Utility_HeaderInclusion');
+		$this->contextIdentifier = $this->configurationBuilder->getContextIdentifier();
+	}
+	
+
+	/**
+	 * @return void
+	 */
+	public function render() {
+
+		$this->headerInclusion->
+
+	}
+
+}
---
+++ Resources/Private/Templates/Rondell/JSTemplates/Rondell.js
@@ -0,0 +1,22 @@
+$(function () {
+    $('#rondellGallery-###contextIdentifier### > div').rondell({
+        preset:'carousel',
+        radius:{
+            x:260
+        },
+        scaling:2.4,
+        isActive:function () {
+            return !$('#fancybox-wrap').is(':visible');
+        }
+    });
+
+    $('#rondellGallery-###contextIdentifier### a').fancybox({
+        hideOnContentClick:false,
+        padding:10,
+        scrolling:'no',
+        overlayColor:'#333',
+        onStart:function (items, idx) {
+            return $(items[idx]).parent().hasClass('rondellItemFocused');
+        }
+    });
+});
