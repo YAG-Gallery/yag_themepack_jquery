cae4c7e288f0b7fff591c2ba2e7e1bd3330b6d76
Daniel Lienert <daniel@lienert.cc>
2015-10-31 19:32:41 +0100

    [TASK] Set maximum version to 7.6

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' => 
     array (
-      'typo3' => '6.2.0-7.1.99',
+      'typo3' => '6.2.0-7.6.99',
       'extbase' => '',
       'fluid' => '',
       'yag' => '4.0.4',
